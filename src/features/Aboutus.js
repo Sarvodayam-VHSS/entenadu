18aa274714aeaadb4c132aa0f14c71b11a4a08a4
psabhishek26 <sploitpay.aosp.test@gmail.com>
2024-02-21 11:36:05 +0530

    Improve About page layouts
    Signed-off-by: psabhishek26 <sploitpay.aosp.test@gmail.com>

--- src/features/Aboutus.js
+++ src/features/Aboutus.js
@@ -8,15 +8,16 @@ const AboutUs = () => {
         source={require("./../../assets/sarvodayam.jpeg")}
         style={styles.image}
       />
+      <Text style={styles.heading}>Welcome to Sarvodayam School</Text>
       <Text style={styles.description}>
         Sarvodayam Vocational Higher Secondary School, Aryampadam was
         established in 1955 by Peeyar Mundathicode, a poet. Although Sarvodayam
         was initially a UP School, it was upgraded to a High School in 1983 and
         a Vocational Higher Secondary School in 2000. Along with a good pass
-        percentage, Sarvodayam also excels in extra-curricular activities. Also,
-        units like ATL (Atal Tinkering Lab), JRC (Junior Red Cross), NSS
-        (National Service Scheme), SPC (Student Police Cadet), Little Kites,
-        Scouts & Guides add to the education.
+        percentage, Sarvodayam also excels in extra-curricular activities. Units
+        like ATL (Atal Tinkering Lab), JRC (Junior Red Cross), NSS (National
+        Service Scheme), SPC (Student Police Cadet), Little Kites, Scouts &
+        Guides add to the holistic education.
       </Text>
     </ScrollView>
   );
@@ -30,10 +31,20 @@ const styles = StyleSheet.create({
   image: {
     width: 400,
     height: 300,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#333",
+    marginBottom: 10,
   },
   description: {
     width: "100%",
-    marginTop: 10,
+    fontSize: 16,
+    color: "#555",
+    lineHeight: 24,
   },
 });
 
